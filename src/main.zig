c2331de82325b0014474493875add075e28f3ceb
NitishNaineni <naineninitish@gmail.com>
2024-08-18 22:20:36 -0500

    added progress bar

--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,22 @@ pub fn main() !void {
     const image_width: usize = 2048;
     const image_height: usize = 2048;
 
+    const parent_progress_node = std.Progress.start(.{});
+    const progress_node = parent_progress_node.start(
+        "rendering pixels",
+        image_width * image_height,
+    );
+    defer progress_node.end();
+
     const stdout = std.io.getStdOut().writer();
     var buffer = std.io.bufferedWriter(stdout);
 
     const bufOut = buffer.writer();
 
-    try bufOut.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
+    try bufOut.print(
+        "P3\n{d} {d}\n255\n",
+        .{ image_width, image_height },
+    );
 
     for (0..image_height) |j| {
         for (0..image_width) |i| {
@@ -21,7 +31,12 @@ pub fn main() !void {
             const ig: u8 = @as(u8, @intFromFloat(255.999 * g));
             const ib: u8 = @as(u8, @intFromFloat(255.999 * b));
 
-            try bufOut.print("{d} {d} {d}\n", .{ ir, ig, ib });
+            try bufOut.print(
+                "{d} {d} {d}\n",
+                .{ ir, ig, ib },
+            );
+
+            progress_node.completeOne();
         }
     }
     try buffer.flush();
